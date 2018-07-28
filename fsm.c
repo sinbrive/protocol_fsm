c841c9be3bff64fb4a62dc15766b2aca06c84c81
sinbrive <sinbrive@gmail.com>
2018-07-28 10:35:16 +0200

    Update fsm.c

--- fsm.c
+++ fsm.c
@@ -1,33 +1,20 @@
-//
-// fsm.c
-// FSM sample code
-//
-// Created by Minsuk Lee, 2014.11.1.
-// Copyright (c) 2014. Minsuk Lee All rights reserved.
-// see LICENSE
-
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/errno.h>
-
 #define TEST_FSM
 
-#define NUM_STATE   3
-#define NUM_EVENT   8
+#define NUM_STATE   4
+#define NUM_EVENT   4
 
-enum pakcet_type { F_CON, F_ACK, F_FIN, F_DATA };       // Packet Type
-enum proto_state { wait_CON, CON_sent, CONNECTED };     // States
+// States
+enum proto_state 
+  { ZERO_BLINK, ONE_BLINK, 
+    TWO_BLINK, THREE_BLINK 
+  };     
 
 // Events
-enum proto_event { RCV_CON, RCV_FIN, RCV_ACK, RCV_DATA, CONNECT, CLOSE, SEND, TIMEOUT };
+enum proto_event { onePUSH, twoPUSH, threePUSH, TIMEOUT };
 
-char *pkt_name[] = { "F_CON", "F_ACK", "F_FIN", "F_DATA" };
-char *st_name[] =  { "wait_CON", "CON_sent", "CONNECTED" };
-char *ev_name[] =  { "RCV_CON", "RCV_FIN", "RCV_ACK", "RCV_DATA",
-                     "CONNECT", "CLOSE",   "SEND",    "TIMEOUT"   };
+char *st_name[] =  { "zeroBlink", "oneBlink", "twoBlink", "threeBlink" };
+
+char *ev_name[] =  { "onePush", "twoPush", "threePush", "TIMEOUT"};
 
 struct state_action {           // Protocol FSM Structure
     void (* action)(void *p);
@@ -40,189 +27,122 @@ struct p_event {                // Event Structure
     int size;
 };
 
-#define MAX_DATA_SIZE   (500)
-struct packet {                 // 504 Byte Packet to & from Simulator
-    unsigned short type;        // enum packet_type
-    unsigned short size;
-    char data[MAX_DATA_SIZE];
-};
+unsigned int stateTimeout=3000;
 
-enum proto_state c_state = wait_CON;         // Initial State
-volatile int timedout = 0;
+enum proto_state c_state = ZERO_BLINK;     // States
 
-static void timer_handler(int signum)
+unsigned long referenceTime=millis();
+
+struct p_event *eventp;
+
+static void entryZeroBlink(void *p)
 {
-    printf("Timedout\n");
-    timedout = 1;
+    stateTimeout = 5000;
+    referenceTime=millis();
+    char const buffer = sprintf("Current Stat = %s\n", st_name[c_state]);
+    Serial.println(buffer);
+    //Serial.println(st_name[ZERO_BLINK]);
 }
 
-static void timer_init(void)
-{
-    struct sigaction sa;
 
-    memset (&sa, 0, sizeof (sa));
-    sa.sa_handler = &timer_handler;
-    sigaction(SIGALRM, &sa, NULL);
+static void entryOneBlink(void *p)
+{
+    stateTimeout = 5000;
+    referenceTime=millis();
+    Serial.println(st_name[ONE_BLINK]);    
 }
 
-void set_timer(int sec)
+static void entryTwoBlink(void *p)
 {
-    struct itimerval timer;
-
-    timedout = 0;
-    timer.it_value.tv_sec = sec;
-    timer.it_value.tv_usec = 0;
-    timer.it_interval.tv_sec = 0;   // Non Periodic timer
-    timer.it_interval.tv_usec = 0;
-    setitimer (ITIMER_REAL, &timer, NULL);
-}
-void send_packet(int flag, void *p, int size)
-{
-#ifdef TEST_FSM
-    printf("SEND %s\n", pkt_name[flag]);
-#else
-    struct packet buf;
-
-    buf.type = flag;
-    if (size) {
-        buf.size = size;
-        memcpy(buf.data, p, (size > MAX_DATA_SIZE) ? MAX_DATA_SIZE : size);
-    }
-    //---> sendto() to SIMULATOR
-#endif
+    stateTimeout = 5000;
+    referenceTime=millis();
+     Serial.println(st_name[TWO_BLINK]);
 }
 
-static void report_connect(void *p)
+static void entryThreeBlink(void *p)
 {
-    printf("Connected\n");
-    set_timer(0);           // Stop Timer
-    // Report connection made to upper layer
+    stateTimeout = 5000;
+    referenceTime=millis();
+     Serial.println(st_name[THREE_BLINK]);
 }
 
-static void passive_con(void *p)
-{
-    send_packet(F_ACK, NULL, 0);
-    report_connect(NULL);
-}
 
-static void active_con(void *p)
-{
-    send_packet(F_CON, NULL, 0);
-    set_timer(3);
-}
-
-static void close_con(void *p)
-{
-    printf("Connection Closed\n");
-    send_packet(F_FIN, NULL, 0);
-    // Report Connected Closed to upper layer
-}
-
-static void send_data(void *p)
-{
-    printf("Send Data to peer size:%d\n", ((struct p_event*)p)->size);
-    send_packet(F_DATA, ((struct p_event *)p)->data, ((struct p_event *)p)->size);
-}
-
-static void report_data(void *p)
-{
-    printf("Data Arrived size:%d\n", ((struct p_event*)p)->size);
-    // Queue received data for upper layer user
-}
 
 struct state_action p_FSM[NUM_STATE][NUM_EVENT] = {
   //  for each event:
-  //  RCV_CON,                    RCV_FIN,                 RCV_ACK,                       RCV_DATA,
-  //  CONNECT,                    CLOSE,                   SEND,                          TIMEOUT
+  //  onePUSH,                    twoPUSH,                 threePUSH,                TIMEOUT
 
-  // - wait_CON state
-  {{ passive_con, CONNECTED }, { NULL, wait_CON },      { NULL, wait_CON },            { NULL, wait_CON },
-   { active_con,  CON_sent },  { NULL, wait_CON },      { NULL, wait_CON },            { NULL, wait_CON }},
+  // - zero push
+  {{ entryOneBlink, ONE_BLINK }, { entryTwoBlink, TWO_BLINK },      { entryThreeBlink, THREE_BLINK },      { NULL, ZERO_BLINK }},
 
-  // - CON_sent state
-  {{ passive_con, CONNECTED }, { close_con, wait_CON }, { report_connect, CONNECTED }, { NULL,      CON_sent },
-   { NULL,        CON_sent },  { close_con, wait_CON }, { NULL,           CON_sent },  { close_con, wait_CON }},
+  // - one push
+  {{ NULL, ONE_BLINK }, { entryTwoBlink, TWO_BLINK },      { entryThreeBlink, THREE_BLINK }, { entryZeroBlink, ZERO_BLINK }},
 
-  // - CONNECTED state
-  {{ NULL, CONNECTED },        { close_con, wait_CON }, { NULL,      CONNECTED },      { report_data, CONNECTED },
-   { NULL, CONNECTED },        { close_con, wait_CON }, { send_data, CONNECTED },      { NULL,        CONNECTED }},
+  // - two push
+  {{ entryOneBlink, ONE_BLINK }, { NULL, TWO_BLINK },      { entryThreeBlink, THREE_BLINK },      { entryZeroBlink, ZERO_BLINK }},
+  
+    // - three push
+  {{ entryOneBlink, ONE_BLINK }, { entryTwoBlink, TWO_BLINK },      { NULL, THREE_BLINK },      { entryZeroBlink, ZERO_BLINK }},
 };
 
 struct p_event *get_event(void)
 {
     static struct p_event event;    // not thread-safe
  
-#ifdef TEST_FSM
-    char test_event[10];
-    int i;
-
-    event.data = NULL;
-    event.size = 0;
-loop:
-    while (scanf("%s", test_event) <= 0) {
-        // scanf() returns error on signal (timer)
-        if ((errno == EINTR) && timedout) { // ALARM signal Catched !!
-            timedout = 0;
-            i = TIMEOUT;
-            goto got_it;
+    #ifdef TEST_FSM
+      
+      event.data = NULL;
+      event.size = 0;
+    
+      if (Serial.available() > 0) 
+      {
+        int number = Serial.parseInt(); 
+        event.event=proto_event((number-1)%2);
+      }
+      else 
+      {
+        if ((millis()-referenceTime)>stateTimeout)
+        {
+           event.event=TIMEOUT;
         }
-    }
-        
-    for (i = 0; i < NUM_EVENT; i++)
-        if (!strcasecmp(test_event, ev_name[i]))
-            goto got_it;
-    if (!strcasecmp(test_event, "quit"))
-        return NULL;
-    fprintf(stderr, "%s : BAD EVENT NAME\n", test_event);
-    goto loop;
-got_it:
-    event.event = i;
-    // No data
-#else
-    // Check if there is user command
-    // Check Packet arrival by event_wait()
-    //    if then, decode header to make event
-    // Check if timer is timed-out
-#endif
+        else return NULL;
+      }
+    #endif
     return &event;
 }
 
-void
-Protocol_Loop(void)
+void setup()
 {
-    struct p_event *eventp;
+  #ifdef TEST_FSM  
+    Serial.begin(9600);
+    Serial.println(st_name[ZERO_BLINK]);
+  #endif
+  
+}
 
-    timer_init();
-    while (1) {
-        printf("Current Stat = %s\n", st_name[c_state]);
-
-        /* Step 0: Get Input Event */
-        if((eventp = get_event()) == NULL)
-            break;
-
+void loop()
+{
+       
+     /* Step 0: Get Input Event */
+    if((eventp = get_event()) != NULL) {
+      
+//        #ifdef TEST_FSM  
+//          char const buffer = printf("Current Stat = %s\n", st_name[c_state]);
+//          //Serial.println(st_name[c_state]);
+//        #endif    
+        
         /* Step 1: Do Action */
-        if (p_FSM[c_state][eventp->event].action)
+        if (p_FSM[c_state][eventp->event].action) {
             p_FSM[c_state][eventp->event].action(eventp);
+        }
         else
-            printf("No Action for this event\n");
-
-        /* Step 2: Set Next State */
-        c_state = p_FSM[c_state][eventp->event].next_state;
+        {
+          #ifdef TEST_FSM  
+//             Serial.print("No Action for this event\n");
+          #endif
+        }             
+         /* Step 2: Set Next State */
+       c_state = p_FSM[c_state][eventp->event].next_state;
+        
     }
 }
-
-int
-main(int argc, char *argv[])
-{
-    // INITIALIZE USER THREAD
-    // SIMULATOR_INITIALIZE
-
-    printf("Entering protocol loop...\n");
-    printf("type 'CONNECT', 'CLOSE', 'SEND', or 'QUIT'\n");
-    Protocol_Loop();
-
-    // SIMULATOR_CLOSE
-
-    return 0;
-}
-
